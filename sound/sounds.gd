a17cd5458374e7c5b0d4ad877966d0f5682a6b97
LayduA <adrien.laydu@gmail.com>
2024-03-09 18:13:32 +0100

    Add sounds

---
+++ sound/sounds.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer
+
+var sounds = {
+	'ouch': load('res://sound/ouch.mp3')
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func play_sound(sound_name):
+	stream = sounds[sound_name]
+	play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
