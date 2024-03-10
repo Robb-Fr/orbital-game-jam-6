990bcd96cd4d534477ba81b4d8d70f7985e193e1
LayduA <adrien.laydu@gmail.com>
2024-03-10 02:39:37 +0100

    LA MUSIQUE DU MENU (ou devrais-je dire MENUSIQUE)

--- sound/music.gd
+++ sound/music.gd
@@ -1,10 +1,24 @@
 extends AudioStreamPlayer
 
-var music: AudioStream
+var game_music = load("res://sound/music_game_wizard_hero.wav")
+var menu_music = load("res://sound/music_menu.wav")
+
+var on_menu = true
 
 func _ready():
-	music = load("res://sound/music_game_wizard_hero.wav")
+	play_menu()
+
+func play_menu() -> void:
+	stream = menu_music
+	play()
+
+
 
 func play_song() -> void:
-	stream = music
+	stream = game_music
 	play()
+
+
+func _on_finished():
+	if stream == menu_music:
+		play()
